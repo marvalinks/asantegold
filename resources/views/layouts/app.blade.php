a43a11137c056f8b0f4fcdef0e0087b025b3433c
qhobeey <bludohric@gmail.com>
2024-02-16 21:13:12 +0000

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,8 +85,8 @@
         <script src="/assets/js/vendors.js"></script>
         <script src="/assets/js/main.js"></script>
         <script src="/stocks/assets/dist/app.js"></script>
-        <script>
+        {{-- <script>
             $('.elementor-button').addClass('g78');
-        </script>
+        </script> --}}
     </body>
 </html>
