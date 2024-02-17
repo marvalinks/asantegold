f552eab100422ff4912ddb3d83f5dc5bec5b9374
qhobeey <bludohric@gmail.com>
2024-02-17 15:22:47 +0000

    .

--- resources/views/pages/stock-info.blade.php
+++ resources/views/pages/stock-info.blade.php
@@ -16,6 +16,43 @@
         <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20" style="text-align: center;">
             <h2 class="text-62 md:text-38 uppercase fw-500"><span class="text-accent-1">Stock Information</span></h2>
         </div>
+        <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20" style="text-align: center;">
+            
+            <div class="tradingview-widget-container">
+                <div class="tradingview-widget-container__widget"></div>
+                {{-- <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div> --}}
+                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js" async>
+                    {
+                        "width": "1920",
+                        "height": 550,
+                        "symbolsGroups": [
+                        {
+                            "name": "Indices",
+                            "originalName": "Indices",
+                            "symbols": [
+                            {
+                                "name": "CSE:ASE",
+                                "displayName": "CSE"
+                            },
+                            {
+                                "name": "FWB:1A9",
+                                "displayName": "FRANKFURT EXCHANGE"
+                            },
+                            {
+                                "name": "OTC:ASGOF",
+                                "displayName": "U.S.O.T.C"
+                            }
+                            ]
+                        }
+                        ],
+                        "showSymbolLogo": true,
+                        "isTransparent": false,
+                        "colorTheme": "light",
+                        "locale": "en"
+                    }
+                </script>
+            </div>
+        </div>
         <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20 ert" style="text-align: center;">
             <div class="col-md-6">
                 <div class="tradingview-widget-container" style="height: 100%; width: 100%;">
@@ -98,47 +135,7 @@
                 </div>
             </div>
         </div>
-        <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20" style="text-align: center;">
-            
-            <div class="tradingview-widget-container">
-                <div class="tradingview-widget-container__widget"></div>
-                {{-- <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div> --}}
-                <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js" async>
-                    {
-                        "width": "1920",
-                        "height": 550,
-                        "symbolsGroups": [
-                        {
-                            "name": "Indices",
-                            "originalName": "Indices",
-                            "symbols": [
-                            {
-                                "name": "CSE:ASE",
-                                "displayName": "CSE"
-                            },
-                            {
-                                "name": "FWB:1A9",
-                                "displayName": "FRANKFURT EXCHANGE"
-                            },
-                            {
-                                "name": "OTC:ASGOF",
-                                "displayName": "U.S.O.T.C"
-                            },
-                            {
-                                "name": "COMEX:GC1!",
-                                "displayName": "GOLD GC1"
-                            }
-                            ]
-                        }
-                        ],
-                        "showSymbolLogo": true,
-                        "isTransparent": false,
-                        "colorTheme": "light",
-                        "locale": "en"
-                    }
-                </script>
-            </div>
-        </div>
+        
     </div>
 </section>
 @endsection
