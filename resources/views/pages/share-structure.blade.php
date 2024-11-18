c0065ae6cecda9f5857c99e9ce3b8be8b69bc57e
qhobeey <bludohric@gmail.com>
2024-11-18 12:59:13 +0000

    .

--- resources/views/pages/share-structure.blade.php
+++ resources/views/pages/share-structure.blade.php
@@ -14,7 +14,7 @@
 <section class="layout-pt-lg layout-pb-lg bg-accent-3 our-projects">
     <div class="container">
         <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20" style="text-align: center;">
-            <h2 class="text-62 md:text-38 uppercase fw-500"><span class="text-accent-1">As of August 2024</span></h2>
+            <h2 class="text-62 md:text-38 uppercase fw-500"><span class="text-accent-1">As of November 2024</span></h2>
         </div>
         <div class="row y-gap-30 mt-60 md:mt-40 sm:mt-20">
             <div class="table -type-2 uppercase text-18 md:text-16">
@@ -47,11 +47,11 @@
                         </tr>
                         <tr>
                             <td>Common Shares</td>
-                            <td>445,384,986</td>
+                            <td>501,086,895</td>
                         </tr>
                         <tr>
                             <td>Stock Options Outstanding</td>
-                            <td>16,102,840</td>
+                            <td>15,074,240</td>
                         </tr>
                         <tr>
                             <td>Warrants Outstanding</td>
@@ -59,15 +59,15 @@
                         </tr>
                         <tr>
                             <td>RSU</td>
-                            <td>6,327,260</td>
+                            <td>7,965,360</td>
                         </tr>
                         <tr>
                             <td>DSU</td>
-                            <td>6,785,900</td>
+                            <td>7,835,900</td>
                         </tr>
                         <tr>
                             <td>Fully Diluted Share Capital</td>
-                            <td>479,600,986</td>
+                            <td>536,962,395</td>
                         </tr>
                     </tbody>
                 </table>
