b00460267bd8c1f41ecc8fff4682d648811b4201
Qhobeey Martison <bludohric@gmail.com>
2026-05-05 17:18:43 +0000

    .

--- resources/views/pages/estmaReports.blade.php
+++ resources/views/pages/estmaReports.blade.php
@@ -34,7 +34,7 @@
             <div class="col-md-6">
                 <ul>
                     <li style="padding-bottom: 20px;">
-                        <a target="_blank" href="https://drive.google.com/file/d/1i5GGQ-w6m-H-ztEbA3L_fAoYj1kFRnPx/view?usp=drive_link" class="button -md -dark-1 bg-accent-1 text-white col-12">
+                        <a target="_blank" href="https://tt3.ams3.digitaloceanspaces.com/asante-gold/2025%20ESTMA%20Report%20-%20Asante%20Gold%20Corporation%20ASE%20E099633.pdf" class="button -md -dark-1 bg-accent-1 text-white col-12">
                             DOWNLOAD  –  ASANTE GOLD ESTMA 2025 REPORT 
                         </a>
                     </li>
